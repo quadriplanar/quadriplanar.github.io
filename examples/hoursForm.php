de7b43781216c06fb961eb1f86f076893c6a831f
quadriplanar <48447695+quadriplanar@users.noreply.github.com>
2019-03-26 14:21:51 -0400

    Create hoursForm.php

---
+++ examples/hoursForm.php
@@ -0,0 +1,12 @@
+<?php
+echo "anything at all";
+/*if($_POST["submit"]) {
+    $recipient="tmoor025@uottawa.ca"; //Enter your mail address
+    $subject="Contact from Website"; //Subject
+    $name=$_POST["name"];
+    $mailBody="Name: $name\n";
+    mail($recipient, $subject, $mailBody);
+    sleep(1);
+    header("Location:http:www.wikipedia.org");
+}*/
+?>
